f6c4419f5494e067d0b7b4c8650b16b924cd45b1
francofil <92694538+francofil@users.noreply.github.com>
2024-03-13 09:09:35 -0300

    Add files via upload

---
+++ Proyecto1/src/Library/CuentaBancaria.cs
@@ -0,0 +1,19 @@
+﻿namespace Library;
+
+public class CuentaBancaria
+{
+    public string Titular {get; set;}
+    public double Saldo {get; set;}  
+
+    public CuentaBancaria(string eltitular, double elsaldo){
+
+        this.Titular = eltitular;
+        this.Saldo = elsaldo;   
+
+    }
+
+    public void Depositar(double monto){
+        this.Saldo += monto;
+    }
+
+}
---
+++ Proyecto1/src/Program/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Library
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args){
+            CuentaBancaria cb1 = new CuentaBancaria("Franco", 10000);
+            cb1.Depositar(100);
+            Console.WriteLine(cb1.Saldo);
+        }
+    }
+
+}
