51c1e60c35900b2572943e8bce4a1c0066de4673
francofil <92694538+francofil@users.noreply.github.com>
2024-03-13 09:01:53 -0300

    Add files via upload

---
+++ PII_Full_GRASP_and_SOLID_2_Start/src/Library/Recipe.cs
@@ -0,0 +1,33 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Recipe
+    {
+        private ArrayList steps = new ArrayList();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Step step)
+        {
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+
+        public ArrayList GetSteps(){
+            return steps;
+        }
+        
+    }
+}
